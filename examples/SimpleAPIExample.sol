f446d603d6340a3066c634b4908159963c46c59b
yoyyyyo <96636207+yoyyyyo@users.noreply.github.com>
2022-03-27 22:19:49 +0200

    replace address in SimpleAPIExample with zero address

--- examples/SimpleAPIExample.sol
+++ examples/SimpleAPIExample.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 // SPDX-License-Identifier: CC0-1.0
 
 contract SimpleAPIExample is ERC721Sync {
-    address ERC721_TOKEN = 0xb7F7F6C52F2e2fdb1963Eab30438024864c313F6;
+    address ERC721_TOKEN = 0x0000000000000000000000000000000000000000;
     constructor() ERC721Sync("Bored Token Speedboat Club", "BTSC", ERC721_TOKEN) {}
 
     function tokenURI(uint _id) external pure override returns (string memory) {
